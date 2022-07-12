f422f00bebbed018940a3abbd8913bc41f504862
Zac Flynn <zacharyflynn06@gmail.com>
2022-07-11 20:46:36 -0600

    add icons, house image and set up tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Mail from "../components/icons/Mail.svelte";
+  import Phone from "../components/icons/Phone.svelte";
+</script>
+
+<div class="bg-four flex justify-end text-one space-x-3 text-xs h-8 pr-3">
+  <div class="flex items-center hover:opacity-70">
+    <div class="px-2">
+      <Phone className="h-3 w-3" />
+    </div>
+    <span>(970) 389-7126</span>
+  </div>
+
+  <a
+    href="mailto:zacharyflynn06@gmail.com"
+    class="flex items-center hover:opacity-70"
+    title="email"
+  >
+    <div class="px-2">
+      <Mail className="h-3 w-3" />
+    </div>
+    <span>tanner@isadouch.com</span>
+  </a>
+</div>
+<div class="relative h-screen w-screen justify-center items-center">
+  <img src="src/public/images/house.jpg" alt="" class="object-contain" />
+  <h1
+    class="absolute top-24 right-1/2 translate-x-1/2 text-7xl font-bold font-oswald text-one "
+  >
+    Cash Buyers Colorado
+  </h1>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        one: "#F0EBE3",
+        two: "#E4DCCF",
+        three: "#7D9D9C",
+        four: "#576F72",
+      },
+      fontFamily: { oswald: ["Oswald", "sans-sarif"] },
+    },
   },
   plugins: [],
 };
