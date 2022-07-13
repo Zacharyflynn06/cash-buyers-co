fc12afb1c3300769f0122a609b7d7aca3d989b46
Zac Flynn <zacharyflynn06@gmail.com>
2022-07-12 21:04:49 -0600

    change section height on mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <header
-  class="bg-four flex justify-end text-one space-x-3 h-8 pr-3 font-oswald text-lg"
+  class="bg-four flex justify-end text-white space-x-3 h-8 pr-3 font-oswald text-lg"
 >
   <a href="tel:970-389-7126" class="flex items-center hover:opacity-70">
     <div class="px-2">
@@ -29,15 +29,15 @@
   <img
     src="/images/house_2.jpeg"
     alt=""
-    class="h-screen object-cover md:w-screen"
+    class="h-96 sm:h-screen object-cover md:w-screen"
   />
 
   <div
-    class="absolute top-0 left-0 text-center bg-black bg-opacity-25 h-screen w-screen"
+    class="absolute top-0 left-0 text-center bg-black bg-opacity-25 h-96 sm:h-screen w-screen"
   />
 
   <div
-    class="absolute text-one top-[15%] md:top-1/3 font-oswald space-y-5 items-center mx-5 md:mx-10 lg:mx-20"
+    class="absolute text-white top-[15%] md:top-1/3 font-oswald space-y-5 items-center mx-5 md:mx-10 lg:mx-20"
   >
     <h1 class="text-8xl font-extrabold ">Cash Buyers CO</h1>
     <p>
@@ -49,43 +49,43 @@
 
 <div class="flex py-20 items-center justify-center bg-four">
   <div class="space-y-5 mx-5 md:mx-10 lg:mx-20">
-    <h1 class="font-oswald font-bold text-2xl text-one">Our Mission</h1>
-    <p class="text-one font-oswald text-lg ">
+    <h1 class="font-oswald font-bold text-2xl text-white">Our Mission</h1>
+    <p class="text-white font-oswald text-lg ">
       At Cash Buyers Co, we strive to get our sellers the fastest and highest
       cash offer possible for their homes and properties. With a 97% closure
       rate on all offers we know we can help sellers make fast and profitable
       decisions in a market that is constantly shifting.
     </p>
-    <p class="text-one font-oswald text-lg">
+    <p class="text-white font-oswald text-lg">
       Cash Buyers CO is here to help sellers close their house with a cash wire
       transfer on the day of closing. No showings, no realtor fees, and no
       inspection fees.
     </p>
-    <p class="text-one font-oswald text-lg">
+    <p class="text-white font-oswald text-lg">
       We take the house in AS IS CONDITION. Meaning we decide on a number and
       you can leave the house as it is including furniture or trash items you do
       not wish to take with your on your next journey.
     </p>
 
-    <p class="text-one font-oswald text-lg">
+    <p class="text-white font-oswald text-lg">
       Give us a call today to receive a no obligation no risk cash offer on your
       home or property!
     </p>
   </div>
 </div>
 
-<div class="h-screen relative justify-center items-center ">
+<div class="h-96 sm:h-screen relative justify-center items-center ">
   <img
     src="/images/house.jpg"
     alt=""
-    class="h-screen object-cover md:w-screen"
+    class="h-96 sm:h-screen object-cover md:w-screen"
   />
   <div
-    class="absolute top-0 left-0 text-center bg-black bg-opacity-50 h-screen w-screen"
+    class="absolute top-0 left-0 text-center bg-black bg-opacity-50 h-96 sm:h-screen w-screen"
   />
 
   <div
-    class="absolute text-one top-[40%] md:top-[35%] font-oswald space-y-10 mx-5 md:mx-10 lg:mx-20"
+    class="absolute text-white top-[40%] md:top-[35%] font-oswald space-y-10 mx-5 md:mx-10 lg:mx-20"
   >
     <p class="text-xl md:text-3xl">
       Call us today for a no risk, no obligation cash offer on your home or
